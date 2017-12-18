e66fe508d15c20e697fdd86b66590bc5d8765ef0
Oleg Matrokhin <oyumatrokhin@avito.ru>
2017-12-18 12:40:42 +0300

    add optional params to baseline

--- public/app/plugins/datasource/graphite/gfunc.js
+++ public/app/plugins/datasource/graphite/gfunc.js
@@ -876,13 +876,14 @@ function (_, $) {
     name: 'baseline',
     category: categories.Special,
     params: [
-      { name: "timeShiftUnit", type: "select", options: ['1d', '7d'] },
+      { name: "timeShiftUnit", type: "select", options: ['7d', '1d'] },
       { name: "timeShiftStart", type: "int" },
-      { name: "timeShiftEnd", type: "int" }
+      { name: "timeShiftEnd", type: "int" },
+      { name: "maxAbsentPercent", type: "int", optional: true },
+      { name: "minAvg", type: "int", optional: true }
     ],
     defaultParams: ['7d', 1, 4]
   });
-
   _.each(categories, function(funcList, catName) {
     categories[catName] = _.sortBy(funcList, 'name');
   });
