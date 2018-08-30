0941cb9a2ac06908cc735e0d2cd3a6ee7ef83c87
Oleg Matrokhin <oyumatrokhin@avito.ru>
2018-08-30 14:50:08 +0300

    increase timeout (2)

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -22,7 +22,7 @@ else
 fi
 
 gem install fpm -v 1.4
-yarn install --pure-lockfile --no-progress --network-timeout 10000
+yarn install --pure-lockfile --no-progress --network-timeout 100000
 
 echo "current dir: $(pwd)"
 
